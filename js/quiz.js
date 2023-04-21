6312467dfa0bfa5c003d22d5e07caa83699cf590
MudaIsCarry <Rickambergen25@gmail.com>
2023-04-21 11:44:44 +0200

    done

--- js/quiz.js
+++ js/quiz.js
@@ -3,30 +3,30 @@ const resultDiv = document.createElement("div");
 resultDiv.classList.add("result");
 
 quizForm.addEventListener("submit", function(e) {
-	e.preventDefault();
+  e.preventDefault();
 
-	let score = 0;
-	const answers = ["b", "b", "a", "c", "a", "b", "b", "c", "a", "a", "c", "a", "a", "b", "b", ];
+  let score = 0;
+  const answers = ["b", "b", "a", "c", "a", "b", "b", "c", "a", "a", "c", "a", "a", "b", "b", ];
 
-	for (let i = 1; i <= 15; i++) {
-		const answer = quizForm[`q${i}`].value;
-		if (answer === answers[i - 1]) {
-			score++;
-		}
-	}
+  for (let i = 1; i <= 15; i++) {
+    const answer = quizForm[`q${i}`].value;
+    if (answer === answers[i - 1]) {
+      score++;
+    }
+  }
 
-	resultDiv.innerHTML = `You scored ${score} out of 15.`;
-	quizForm.appendChild(resultDiv);
+  resultDiv.innerHTML = `You scored ${score} out of 15.`;
+  quizForm.appendChild(resultDiv);
 
-	if (score >= 1) {
-		const img = document.createElement("img");
-		img.src = "/img/yes.png";
-		img.alt = "Congratulations, you passed!";
-		quizForm.appendChild(img);
-	} else {
-		const img = document.createElement("img");
-		img.src = "/img/no.png";
-		img.alt = "Sorry, you didn't pass.";
-		quizForm.appendChild(img);
-	}
+  const img = document.createElement("img");
+  img.alt = "Result Image";
+  quizForm.appendChild(img);
+
+  if (score >= 1) {
+    img.src = "/img/yes.png";
+    img.classList.add("full-screen-image");
+  } else {
+    img.src = "/img/no.png";
+    img.classList.add("full-screen-image");
+  }
 });
